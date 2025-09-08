a29d4e722a5830b3753db296faa2b9b1116d38a8
jasonenkristo123 <jasonenkristo@gmail.com>
2025-09-08 23:10:06 +0700

    tugas konversi suhu

--- src/Latihan/Nugas/Belanja.java
+++ src/Latihan/Nugas/Belanja.java
@@ -8,12 +8,15 @@ public class Belanja {
         int tanggalBeli = input.nextInt();
         int bulanBeli = input.nextInt();
         int tahunBeli = input.nextInt();
+
         input.nextLine();
+
         String belanjaBarang = input.nextLine();
         int jumlahBarang = input.nextInt();
         double hargaBarang = input.nextDouble();
         float diskon = input.nextFloat();
         boolean jenisPembayaran = input.nextBoolean();
+        
         input.close();
 
         System.out.println("========== List Barang ==========");
@@ -21,7 +24,7 @@ public class Belanja {
         System.out.printf("%-17s: %s\n", "Barang Belanja ", belanjaBarang);
         System.out.printf("%-17s: %d\n", "Jumlah Barang ", jumlahBarang);
         System.out.printf("%-17s: %,.2f\n", "Harga Barang ", hargaBarang);
-        System.out.printf("%-17s: %.2f%%\n", "Diskon", diskon);
+        System.out.printf("%-17s: %.1f%%\n", "Diskon", diskon);
         System.out.println("---------------------------------");
         System.out.printf("%-17s: %,.2f\n", "Total harga ", jumlahBarang * hargaBarang);
         System.out.printf("%-17s: %,.2f\n", "Pembayaran ", jumlahBarang * hargaBarang - (jumlahBarang * hargaBarang * diskon / 100));
---
+++ src/Latihan/Nugas/Konversi.java
@@ -0,0 +1,14 @@
+package Latihan.Nugas;
+
+import java.util.Scanner;
+
+public class Konversi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("masukkan berapa derajat celcius: ");
+        float celcius = input.nextFloat();
+        input.close();
+        float fahrenheit = (celcius * 9 / 5 + 32);
+        System.out.println("termometer menunjukkan " + fahrenheit + " derajat fahrenheit");
+    }
+}
