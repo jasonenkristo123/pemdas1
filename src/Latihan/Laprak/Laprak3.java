a47d5cd0aa93f290b772a2c048e9abc8abaf6a43
Jason Enkristo <jasonenkristo@gmail.com>
2025-09-26 18:08:46 +0700

    laprak 3 hitung volume

--- src/Latihan/Laprak/Laprak3.java
+++ src/Latihan/Laprak/Laprak3.java
@@ -18,3 +18,92 @@ public class Laprak3 {
         }
     }
 }
+
+class volume {
+    public static void main(String[] args) {
+        boolean kodeValid = true;
+        float pi = 22/7;
+
+        System.out.println("MEMU");
+        System.out.println("0. KELUAR");
+        System.out.println("1. HITUNG VOLUME BALOK");
+        System.out.println("2. HITUNG VOLUME BOLA");
+        System.out.println("3. HITUNG VOLUME KERUCUT");
+        System.out.println("4. HITUNG VOLUME SILINDER");
+        System.out.println("5. HITUNG VOLUME LIMAS SEGITIGA");
+
+        while (kodeValid) {
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("MASUKKAN PILIHAN ANDA: ");
+            int pilihan = input.nextInt();
+
+            switch (pilihan) {
+                case 1:
+                    System.out.println("Menghitung Volume Balok");
+                    System.out.println("Masukkan panjang: ");
+                    float panjang = input.nextFloat();
+                    
+                    System.out.println("Masukkan lebar: ");
+                    float lebar = input.nextFloat();
+
+                    System.out.println("Masukkan tinggi: ");
+                    float tinggiBalok = input.nextFloat();
+
+                    float volumeBalok = panjang * lebar * tinggiBalok;
+                    System.out.println("Volume Balok: " + volumeBalok);
+                break;
+                case 2: 
+                    System.out.println("Menghitung Volume Bola");
+                    System.out.println("Masukkan jari-jari: ");
+                    float jariBola = input.nextFloat();
+
+                    float volumeBola = (4f / 3f) * pi * jariBola * jariBola * jariBola;
+                    System.out.println("Volume Bola: " + volumeBola);
+                break;
+                case 3: 
+                    System.out.println("Menghitung Volume Kerucut");
+                    System.out.println("Masukkan jari-jari: ");
+                    float jarikerucut = input.nextFloat();
+                    
+                    System.out.println("Masukkan tinggi: ");
+                    float tinggikerucut = input.nextFloat();
+
+                    float volumeKerucut = (1f / 3f) * pi * jarikerucut * jarikerucut * tinggikerucut;
+                    System.out.println("Volume Kerucut: " + volumeKerucut);
+                break;
+                case 4:
+                    System.out.println("Menghitung Volume Silinder");
+                    System.out.println("Masukkan jari-jari: ");
+                    float jarisilinder = input.nextFloat();
+                    
+                    System.out.println("Masukkan tinggi: ");
+                    float tinggisilinder = input.nextFloat();
+
+                    float volumeSilinder = pi * jarisilinder * jarisilinder * tinggisilinder;
+                    System.out.println("Volume Silinder: " + volumeSilinder);
+                break;
+                case 5:
+                    System.out.println("Menghitung Volume Limas Segitiga");
+                    System.out.println("Masukkan alas segitiga: ");
+                    float alas = input.nextFloat();
+                    
+                    System.out.println("Masukkan tinggi segitiga: ");
+                    float tinggisegitiga = input.nextFloat();
+                    
+                    System.out.println("Masukkan tinggi limas: ");
+                    float tinggiLimas = input.nextFloat();
+                    float alasSegitiga = (1f / 2f) * alas * tinggisegitiga;
+                    float volumeLimas = (1f / 3f) * alasSegitiga * tinggiLimas;
+                    System.out.println("Volume Limas Segitiga: " + volumeLimas);
+                break;
+                case 0:
+                    System.out.println("Program selesai terimakasih");
+                    kodeValid = false;
+                    break;
+                default:
+                    System.out.println("kode tidak valid");
+            }  
+        }
+    }
+}
